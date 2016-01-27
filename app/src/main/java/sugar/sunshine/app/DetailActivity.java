88fb6fb198e195c543afbb6839728f33693853f8
Armando M. Sudi <armando.sudi@gmail.com>
2016-01-27 16:32:42 +0530

    Initial commit

---
+++ app/src/main/java/sugar/sunshine/app/DetailActivity.java
@@ -0,0 +1,146 @@
+package sugar.sunshine.app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "DetailACtivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        else if (id == R.id.action_view_location) {
+           openPreferredLocationInMap();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        // getting the location stored location in the shared preferences
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+        // building the
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        mapIntent.setData(geoLocation);
+        if(mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+        else {
+            Log.d(LOG_TAG, "Couldnt open " + location);
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class DetailFragment extends Fragment {
+
+        private final String INTENT_TEXT = "intent_text_value";
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private String forecast;
+        private static final String FORECAST_SHARE_HASHTAG = " #Sunshine";
+        private ShareActionProvider shareActionProvider;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            TextView forecastTV = (TextView) rootView.findViewById(R.id.forecast_text);
+            Intent intent = getActivity().getIntent();
+            if( intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                Toast.makeText(getActivity().getApplicationContext(),
+                        " " + forecast,
+                        Toast.LENGTH_LONG).show();
+                forecastTV.setText(" " + forecast);
+            }
+            return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.detailfragment, menu);
+            // locate the menu item with the ShareActionProvider then hold onto it
+            // to set/change the share intent
+            MenuItem item = menu.findItem(R.id.action_share_item);
+            shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+            // Attach an intent to this shareActionProvider, you can update at any time,
+            // like when the user select a new piece of data they might like to share
+            if(shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "ShareActionProvider is null");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            // to return to our application after sharing instead of staying in the app
+            // we are using for sharing
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+    }
+
+}
